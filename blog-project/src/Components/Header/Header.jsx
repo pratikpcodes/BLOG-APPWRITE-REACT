fd058acd2b83b0cac326484ef0a96e664ef0db18
pratikpcodes <pratikkpcodes@gmail.com>
2023-12-17 21:42:32 +0530

    React components

--- blog-project/src/Components/Header/Header.jsx
+++ blog-project/src/Components/Header/Header.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React from "react";
+import { Container, Logo, logoutbtn } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Logoutbtn from "./Logoutbtn";
 
 function Header() {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
+
   return (
-    <div
-    className='bg-slate-400'
-    >Header</div>
-  )
+    <header className="bg-slate-400">
+      Header
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="./">
+              <Logo />
+            </Link>
+          </div>
+          <ul className="flex-ml-auto">
+            {navItems.map((items) => {
+              if (items.active) {
+                <li key={items.name}>
+                  <button onClick={() => navigate(items.slug)}>
+                    {items.name}
+                  </button>
+                </li>; 
+              } else return null;
+            })}
+          {
+            authStatus &&(
+            <li>
+              <Logoutbtn/>
+            </li>
+            )
+          }
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- blog-project/src/Components/index.js
+++ blog-project/src/Components/index.js
@@ -1,2 +1,14 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./Container/Container"
+import logoutbtn from "./Header/Logoutbtn"
+import Logo from "./Logo"
+
+export {
+
+    Header,
+    Footer,
+    Container,
+    Logo,
+    logoutbtn
+}
